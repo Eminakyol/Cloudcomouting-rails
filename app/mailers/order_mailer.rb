91a19d3bcec28e1213303088bfe5e2ec05b19e6c
Muhammed Emin Akyol <meminakyol3@gmail.com>
2023-05-31 14:05:43 +0300

    mailers fixed

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,9 +1,8 @@
 class OrderMailer < ApplicationMailer
 
-
     def notify_on_new_order(order_params)
         @order = order_params
-        mail(to: ENV['GMAIL_ADRESS'], subject: "New Order Received")
-        mail(to: @order[:email], subject: "New Order Received")
+        mail(from: ENV['GMAIL_ADDRESS'], to: ENV['GMAIL_ADDRESS'], subject: "New Order Received")
+        mail(from: ENV['GMAIL_ADDRESS'], to: @order[:email], subject: "New Order Received")
     end
 end
